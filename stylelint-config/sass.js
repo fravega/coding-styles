fa3795d1c30553f3042fbc1597769df6c6a613bd
Facundo Viale <facundo.m.viale@gmail.com>
2017-05-29 12:13:23 -0300

    Update sass rule

--- stylelint-config/sass.js
+++ stylelint-config/sass.js
@@ -4,7 +4,7 @@ module.exports = {
   'plugins': [ 'stylelint-scss' ],
   'rules': {
     'scss/at-import-no-partial-leading-underscore': true,
-    'scss/at-mixin-no-argumentless-call-parentheses': 'never',
+    'scss/at-mixin-argumentless-call-parentheses': 'never',
     'scss/dollar-variable-no-missing-interpolation': true,
     'scss/selector-no-redundant-nesting-selector': true,
     'scss/double-slash-comment-empty-line-before': 'always',
